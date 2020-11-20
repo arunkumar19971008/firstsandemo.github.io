bc45094dc54f5c81fdcb948d0b3231115d71a445
arunkumar19971008 <74770935+arunkumar19971008@users.noreply.github.com>
2020-11-20 17:19:32 +0530

    Friestsandemo

---
+++ precache-manifest.d70c1f232748a281e4199474d8e195be.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efc39f4e09f1c98243c",
+    "url": "/css/app.9aba6170.css"
+  },
+  {
+    "revision": "61e6f1b35c757a50b6c8",
+    "url": "/css/chunk-34c1b436.49c2ae62.css"
+  },
+  {
+    "revision": "1665224273c94b9cf535",
+    "url": "/css/chunk-4156981d.674124d8.css"
+  },
+  {
+    "revision": "4338b8b7eb5d6c3033d4",
+    "url": "/css/chunk-4ba9a0db.20be0cb2.css"
+  },
+  {
+    "revision": "7c0a9935dd400d7f7224",
+    "url": "/css/chunk-vendors.f068ff87.css"
+  },
+  {
+    "revision": "f3cfc611d797c21a8d24c23e866dc227",
+    "url": "/img/banner/hand-sanitizer.webp"
+  },
+  {
+    "revision": "c781bf2830cb600a9eff8091a37bd914",
+    "url": "/img/banner/hand-wash.webp"
+  },
+  {
+    "revision": "530d403564c900994867f008a2fbdb8b",
+    "url": "/img/banner/mosquito-repellent.webp"
+  },
+  {
+    "revision": "489b2d429ae396252f77e69f213b37f8",
+    "url": "/img/banner/sanitizing-wipes.webp"
+  },
+  {
+    "revision": "f6e7e45e40413e33f7c158a59c4d1a3e",
+    "url": "/img/banner/spc.webp"
+  },
+  {
+    "revision": "0e53c88b7c536f4b7809525d4b921b80",
+    "url": "/img/banner/surface-wash.webp"
+  },
+  {
+    "revision": "f00eeae04327a5ca1c3b11ae1fb5d69d",
+    "url": "/img/banner/toilet-seat.webp"
+  },
+  {
+    "revision": "91d0de875dcee7b4669c92a192deca95",
+    "url": "/img/banner/veggie.webp"
+  },
+  {
+    "revision": "56151d22c7945db6126fd02625df6a63",
+    "url": "/img/gallery/FacebookArtboard 1 copy 7.jpg"
+  },
+  {
+    "revision": "98754987732b0ac4c02d65ee8ee51c24",
+    "url": "/img/gallery/FacebookArtboard 1.jpg"
+  },
+  {
+    "revision": "16c32d52fa071e0d5e32a7c0877a04cb",
+    "url": "/img/gallery/FacebookArtboard 2 copy 3.jpg"
+  },
+  {
+    "revision": "b0bc5fbc083c626b59df7215063e3283",
+    "url": "/img/gallery/FirstSanArtboard 1 copy 2.jpg"
+  },
+  {
+    "revision": "46d9f1ef98faf96b3d48c2975585785d",
+    "url": "/img/gallery/FirstSanArtboard 1 copy 3.jpg"
+  },
+  {
+    "revision": "df12e16b406c0f17bac8dbe4e1c0706f",
+    "url": "/img/gallery/FirstSanArtboard 1 copy 4.jpg"
+  },
+  {
+    "revision": "0a900537c6299d22416f1c1f27f005a8",
+    "url": "/img/gallery/FirstSanArtboard 1 copy 6.jpg"
+  },
+  {
+    "revision": "d56656497585e7c3517b45b6a2bd47cb",
+    "url": "/img/gallery/FirstSanArtboard 1 copy 7_1.jpg"
+  },
+  {
+    "revision": "a6ad89194cc0800df2a78f737ee1ea58",
+    "url": "/img/gallery/FirstSanArtboard 1 copy.jpg"
+  },
+  {
+    "revision": "47b36de11f5eb61fe851ac3243321382",
+    "url": "/img/gallery/FirstSanArtboard 1.jpg"
+  },
+  {
+    "revision": "9288931bc1011ea25a3f97263269b0a4",
+    "url": "/img/gallery/FirstSanArtboard 2 copy 3.jpg"
+  },
+  {
+    "revision": "c30ee467748718f147821419c11214e1",
+    "url": "/img/gallery/FirstSanArtboard 2 copy 4.jpg"
+  },
+  {
+    "revision": "93e6ed55c9384be4de94417331a05847",
+    "url": "/img/gallery/FirstSanArtboard 2.jpg"
+  },
+  {
+    "revision": "ac7d472b9ed8f28ba2411b0bcf787b95",
+    "url": "/img/logo-white-cropped.webp"
+  },
+  {
+    "revision": "8d6c82531ab104871014afbbc0ea35e6",
+    "url": "/img/products-collage/Hand Santizer.jpg"
+  },
+  {
+    "revision": "294a19dd7692cdb25bfa43cc46bec257",
+    "url": "/img/products-collage/Hand Santizer.webp"
+  },
+  {
+    "revision": "5deaa31e040c01971c9848087c7e0c3a",
+    "url": "/img/products-collage/Hand wash.jpg"
+  },
+  {
+    "revision": "bde04cb0792a0d7f77b09ba038df38de",
+    "url": "/img/products-collage/Hand wash.webp"
+  },
+  {
+    "revision": "15349203b41aadc2a7e553dba4bafe3e",
+    "url": "/img/products-collage/Mosquito Repellent.jpg"
+  },
+  {
+    "revision": "fa439b4bb2a8b52a00dad640709d9772",
+    "url": "/img/products-collage/Mosquito Repellent.webp"
+  },
+  {
+    "revision": "2ac6088c6ddf6b128df8c938622b56b0",
+    "url": "/img/products-collage/SPC.jpg"
+  },
+  {
+    "revision": "ce0432c00412d93c7ffb3036eec2170e",
+    "url": "/img/products-collage/SPC.webp"
+  },
+  {
+    "revision": "23536396321401e3b8e38d520ed1d847",
+    "url": "/img/products-collage/Sanitizing Wipes.jpg"
+  },
+  {
+    "revision": "6b5b1f2891b9c47b69f364df3eb20c39",
+    "url": "/img/products-collage/Sanitizing Wipes.webp"
+  },
+  {
+    "revision": "47945d9bd47a567731130d008c3765e3",
+    "url": "/img/products-collage/Surafce wash.jpg"
+  },
+  {
+    "revision": "346af5826371d6f0b737fd0d50e778a0",
+    "url": "/img/products-collage/Surafce wash.webp"
+  },
+  {
+    "revision": "794b76ba4f74bc7e1f3a1d04fb1434a3",
+    "url": "/img/products-collage/Toilet Seat.jpg"
+  },
+  {
+    "revision": "7990cf519a71539d2304bc32a88def96",
+    "url": "/img/products-collage/Toilet Seat.webp"
+  },
+  {
+    "revision": "bd7087c277c068645e1a316f2bece1a7",
+    "url": "/img/products-collage/Veggie.jpg"
+  },
+  {
+    "revision": "0dedc0e06f3c8b40d5966e3324db6e24",
+    "url": "/img/products-collage/Veggie.webp"
+  },
+  {
+    "revision": "78ffca0249081eb0a0fddbb23399195e",
+    "url": "/img/products-collage/Web Banner.jpg"
+  },
+  {
+    "revision": "7f990f50e8766c5f76ee29e10851a6eb",
+    "url": "/img/products/1 ml Hand Sanitizer Pouch.webp"
+  },
+  {
+    "revision": "705b2263a65f2180b845fbd6fcd8e0ce",
+    "url": "/img/products/1 ml Hand Sanitizer Sachet.webp"
+  },
+  {
+    "revision": "788a1dde258d6d65d41bdcb9d5c4edeb",
+    "url": "/img/products/100 ml hand Sanitizer gel.webp"
+  },
+  {
+    "revision": "1e9802228314c1cb938d4e8623c69b0e",
+    "url": "/img/products/2 ml Hand Sanitizer Pouch.webp"
+  },
+  {
+    "revision": "40789d4da11124fd8e070d4d028ab44a",
+    "url": "/img/products/2 ml Hand Sanitizer Sachet.webp"
+  },
+  {
+    "revision": "10c303c28da5203b039751b7f6b8f802",
+    "url": "/img/products/25 ml hand Sanitizer gel.webp"
+  },
+  {
+    "revision": "f88aa8c32c20ed25eceb7a7426bb50c4",
+    "url": "/img/products/50 & 100 ml Mosquito Repellent Spray.webp"
+  },
+  {
+    "revision": "afdef9ae2998a7a3c6f0d48e4d8e0c72",
+    "url": "/img/products/50 & 100 ml Surface Spray.webp"
+  },
+  {
+    "revision": "783569c1fef3283bda2f5051986ef55d",
+    "url": "/img/products/50 & 100 ml Toilet Seat Spray.webp"
+  },
+  {
+    "revision": "cff624eaebd9cc4194df9973ff0e3d84",
+    "url": "/img/products/50 & 100 ml hand Sanitizer Spray.webp"
+  },
+  {
+    "revision": "160701ead25f4c9bb947884edba069db",
+    "url": "/img/products/500 ml Hand Sanitizer.webp"
+  },
+  {
+    "revision": "f850575c1cf4598b4609c77d0d000be7",
+    "url": "/img/products/500 ml Surface Spray.webp"
+  },
+  {
+    "revision": "c210e862e223b076e3eb372310f67a13",
+    "url": "/img/products/500ml veggie and fruit wash.webp"
+  },
+  {
+    "revision": "8ca5ad71390c48796ead2642c4d4243a",
+    "url": "/img/products/5L Hand Sanitizer Can.webp"
+  },
+  {
+    "revision": "051e4d3ee6414484f395b2c065132f17",
+    "url": "/img/products/5L Surface Sanitizer Can.webp"
+  },
+  {
+    "revision": "eedeb399c76e44208c8b53bdbe509a70",
+    "url": "/img/products/Hand Sanitizer Card Spray.webp"
+  },
+  {
+    "revision": "cb23205abd140b38adde8755644e3097",
+    "url": "/img/products/Hand Wash Pouch.webp"
+  },
+  {
+    "revision": "5109c1bb334df7dc2ec0bb4fe45bd8a0",
+    "url": "/img/products/Hand Wash Sachet.webp"
+  },
+  {
+    "revision": "9887e1ea6c4509e6359078ae6973d8ef",
+    "url": "/img/products/Mosquito Repellent Pouch.webp"
+  },
+  {
+    "revision": "d886f82c967f84e96408445aed4f264a",
+    "url": "/img/products/Mosquito Repellent Sachet.webp"
+  },
+  {
+    "revision": "e5176b1b34aecd3f34b2ffd69def3bf9",
+    "url": "/img/products/SPC.webp"
+  },
+  {
+    "revision": "f2a1f6411e9826dfd3c3d65446b3fcc0",
+    "url": "/img/products/Wipes Pouch.webp"
+  },
+  {
+    "revision": "dbd994a659a9a20e46702cbeace806c4",
+    "url": "/img/products/Wipes Sachet.webp"
+  },
+  {
+    "revision": "9cc74241a18587851708f2c80bcb9d0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8efc39f4e09f1c98243c",
+    "url": "/js/app.a493f245.js"
+  },
+  {
+    "revision": "5cea1583841da0b17386",
+    "url": "/js/chunk-2d0af10a.c6014837.js"
+  },
+  {
+    "revision": "e580f70086068ea7c234",
+    "url": "/js/chunk-2d21de4f.223b1b9d.js"
+  },
+  {
+    "revision": "61e6f1b35c757a50b6c8",
+    "url": "/js/chunk-34c1b436.44c7d871.js"
+  },
+  {
+    "revision": "1665224273c94b9cf535",
+    "url": "/js/chunk-4156981d.7197d592.js"
+  },
+  {
+    "revision": "4338b8b7eb5d6c3033d4",
+    "url": "/js/chunk-4ba9a0db.f2b1a41c.js"
+  },
+  {
+    "revision": "7c0a9935dd400d7f7224",
+    "url": "/js/chunk-vendors.abb10f95.js"
+  },
+  {
+    "revision": "90f3cd8255927da3f6d3853f658f9e17",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
